6819b31455cbefee8f655ebba21ebbfe6cbbf1f9
Lucas Bieniek <lbienieck@hotmail.com>
2025-10-05 00:17:49 -0300

    feat: Cria tela inicial com perfil do usuário
    Cria a tela inicial `MainScreen` utilizando Jetpack Compose.
    
    A tela exibe as seguintes informações do perfil:
    - Imagem de perfil circular.
    - Nome
    - Usuário do Github
    - Bio

--- app/src/main/java/br/com/bnk/devhub/MainActivity.kt
+++ app/src/main/java/br/com/bnk/devhub/MainActivity.kt
@@ -4,9 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.bnk.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,9 +28,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             DevHubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    MainScreen()
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreen() {
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Image(
+            painter = painterResource(R.drawable.eu),
+            contentDescription = "",
+            modifier = Modifier.clip(
+                CircleShape
+            )
+        )
+        Spacer(Modifier.size(16.dp))
+        Text(text = "Nome: Lucas Bieniek")
+        Text(text = "Github: bienieklucas")
+        Text(text = "Bio: Dev android a alguns anos")
     }
 }
